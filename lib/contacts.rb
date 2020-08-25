5189b7be06d62891bbadbe341fbc4d084d5ffb75
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-25 02:26:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,7 +20,7 @@ def remove_strawberry(contacts)
       if key2 == favorite_ice_cream_flavors
         key2.each do |flavor|
         if flavor == "strawberry"
-          flavor.delete
+          key2.delete
         
 
 end
